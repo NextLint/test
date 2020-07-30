dec1cb5bb3bf746400b4fb3d927ddf426df5d629
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-07-30 14:32:35 +0300

    feat(theme): переопределить box shadow у MuiCard

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,3 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export const theme = createMuiTheme({});
+export const theme = createMuiTheme({
+  overrides: {
+    MuiCard: {
+      root: {
+        boxShadow: '0px 0px 12px 0px rgba(50, 50, 50, 0.13)',
+      },
+    },
+  },
+});
