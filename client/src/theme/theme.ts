30aed19cb7f41430addab3c78782cadb64f7af3b
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-07-30 15:21:19 +0300

    feat(theme): переопределить стили MuiAvatar

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -7,5 +7,14 @@ export const theme = createMuiTheme({
         boxShadow: '0px 0px 12px 0px rgba(50, 50, 50, 0.13)',
       },
     },
+    MuiAvatar: {
+      root: {
+        height: '56px',
+        width: '56px',
+      },
+      colorDefault: {
+        backgroundColor: '00A6B4',
+      },
+    },
   },
 });
