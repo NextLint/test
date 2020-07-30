eb75a5f4defbddc6907cd92769ab66090f22ade4
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-07-30 22:07:11 +0300

    fix(client): исправить отображение layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { theme } from './theme/theme';
-import UserCard from './components/card-default';
-import ButtonAdd from './components/button-default';
 
 function App() {
   return (
