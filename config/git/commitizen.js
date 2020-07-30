33120c87758fe3e22b83944266197a3037705fcf
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-07-30 11:53:52 +0300

    build(project): инициализировать проект
    - удаление лишних файлов, созданных create-react-app
    - добавление dev зависимостей husky,
    @commitlint/cli, cz-customizable для стандартизации и проверки сообщений коммитов до отправки в
    репозиторий
    - создание конфигурационных файлов для вышеописанных зависимостей в config/git/
    -
    добавление dev зависимости prettier для стандартизации code-style
    - добавление файла
    .prettierrc.json

---
+++ config/git/commitizen.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:      Добавление нового функционала' },
+    { value: 'fix', name: 'fix:       Исправление ошибок' },
+    { value: 'perf', name: 'perf:      Изменения направленные на улучшение производительности' },
+    { value: 'test', name: 'test:      Добавление тестов' },
+    { value: 'build', name: 'build:     Сборка проекта или изменения внешних зависимостей' },
+  ],
+
+  // TODO: Сделать область видимости для backend
+  scopes: [{ name: 'components' }, { name: 'redux' }],
+
+  messages: {
+    type: 'Какие изменения вы вносите?',
+    scope: '\nВыберите изменяему область кода (опционально):',
+    customScope: 'Укажите свою область:',
+    subject: '\nОписание изменения в повелительном наклонении:\n',
+    body: 'Подробно опишите изменение (опционально). Используйте "|" для новой строки:\n',
+    confirmCommit: 'Принять изменения?',
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: false,
+  subjectLimit: 72,
+};
