189cdcb620c91c690af141299957d2821aea4ecf
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-07-31 21:08:04 +0300

    fix(models): удалить _id из схемы

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
-  _id: Types.ObjectId,
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
