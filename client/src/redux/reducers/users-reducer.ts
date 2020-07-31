98cad948872cf1c7a17f06f2011f695b77dba4f3
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-08-01 01:03:10 +0300

    feat(redux): добавить set-users и add-user cases

--- client/src/redux/reducers/users-reducer.ts
+++ client/src/redux/reducers/users-reducer.ts
@@ -1,4 +1,4 @@
-import { IUsersReducer } from '../../types';
+import { IUsersReducer, IUser } from '../../types';
 
 const initialState: IUsersReducer = {
   users: [],
@@ -7,8 +7,25 @@ const initialState: IUsersReducer = {
 const users = (state = initialState, action: any): IUsersReducer => {
   switch (action.type) {
     case 'SET-USERS':
+      const users = action.users.map((user: IUser, index: number) => ({
+        id: index + 1,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+      }));
       return Object.assign({}, state, {
-        users: action.users,
+        users: users,
+      });
+    case 'ADD-USER':
+      const id = state.users.length + 1;
+      const user = {
+        id: id,
+        first_name: action.user.first_name,
+        last_name: action.user.last_name,
+        email: action.user.email,
+      };
+      return Object.assign({}, state, {
+        users: [...state.users, user],
       });
     default:
       return state;
