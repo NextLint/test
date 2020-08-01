c802263dceed5ac16e00cae7f9be189ecb842597
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-08-01 15:11:31 +0300

    feat(backend): добавить настройку запуска для production

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('config');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -9,6 +10,14 @@ app.use(express.json({ extended: true }));
 let userRoutes = require('./routes/users.routes');
 
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = config.get('port') || 5000;
 
