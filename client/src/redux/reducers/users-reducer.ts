034b15e88ea8a99d430895de1016630ab94c7946
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-08-03 13:46:57 +0300

    fix(users-reducer): изменить логику добавления в соответсвии с типом

--- client/src/redux/reducers/users-reducer.ts
+++ client/src/redux/reducers/users-reducer.ts
@@ -8,7 +8,8 @@ const users = (state = initialState, action: IUsersReducerActions): IUsersReduce
   switch (action.type) {
     case 'SET-USERS':
       const users = action.users.map((user: IUserServerType, index: number) => ({
-        id: index + 1,
+        id: user._id,
+        num: index + 1,
         firstName: user.first_name,
         lastName: user.last_name,
         email: user.email,
@@ -17,9 +18,10 @@ const users = (state = initialState, action: IUsersReducerActions): IUsersReduce
         users: users,
       });
     case 'ADD-USER':
-      const id = state.users.length + 1;
+      const num = state.users.length + 1;
       const user = {
-        id: id,
+        id: action.user._id,
+        num,
         firstName: action.user.first_name,
         lastName: action.user.last_name,
         email: action.user.email,
